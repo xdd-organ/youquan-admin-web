a0478a7ef5b34dacf01de4d8fe06d2da48400e42
tyetao <765068439@qq.com>
2019-01-31 22:25:51 +0800

    商户查询转账凭证

--- src/components/product/product.vue
+++ src/components/product/product.vue
@@ -3,7 +3,7 @@
         <div class="product_center product_list">
             <ul>
                 <li>产品中心</li>
-                <li v-for="item in product_list">{{item}}</li>
+                <li v-for="item in product_list">{{item}} <Button :size="delete_size" type="error" class="delete_product">删除</Button></li>
                 <li><Button type="info" @click="show_product_list = true">新增产品中心</Button></li>
             </ul>
             <Modal
@@ -14,7 +14,27 @@
                 名称：<Input v-model="temp_product_name" placeholder="新增产品中心" style="width:300px;" />
             </Modal>
         </div>
-        <div class="product_center product_category">产品类目</div>
+        <div class="product_center product_category">
+            <div class="product_title_list product_title_list_first">
+                <div class="product_title title_list_one">产品名称</div>
+                <div class="product_title title_list_two">产品明细</div>
+            </div>
+            <div class="product_title_list">
+                <div class="product_title title_list_one">11</div>
+                <div class="product_title title_list_two">22</div>
+            </div>
+            <div class="create_category">
+                <Button type="info" @click="show_product_category = true">新增产品中心</Button>
+                <Modal
+                        v-model="show_product_category"
+                        title="新增产品内容"
+                        @on-ok="ok"
+                        @on-cancel="cancel">
+                    产品名称：<Input v-model="temp_product_name" placeholder="新增产品名称" style="width:300px;" /><br/>
+                    类目详细：<Input v-model="temp_product_name" placeholder="新增类目详细" style="width:300px;margin-top: 20px" />
+                </Modal>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -24,6 +44,8 @@
         data () {
             return {
                 show_product_list: false,
+                show_product_category: false,
+                delete_size: "small",
                 temp_product_name: "",
                 product_list: [
                     "电源管理",
@@ -70,11 +92,43 @@
             li:hover {
                 background: white;
                 font-weight: bold;
-                cursor: pointer;
+                /*cursor: pointer;*/
+            }
+            .delete_product {
+                float: right;
+                margin-top: 5px;
+                margin-right: 20px;
             }
         }
         .product_category {
-
+            width: 80%;
+            .product_title_list {
+                font-size: 13px;
+                .product_title {
+                    height: 30px;
+                    line-height: 30px;
+                    float: left;
+                }
+                .title_list_one {
+                    width: 20%;
+                }
+                .title_list_two {
+                    width: 80%;
+                }
+            }
+            .product_title_list_first {
+                font-size: 15px;
+                font-weight: bold;
+                color: black;
+            }
+            .product_title_list:hover {
+                cursor: pointer;
+                font-weight: bold;
+                /*background-color: white;*/
+            }
+            .create_category {
+                padding-left: 15%;
+            }
         }
     }
 
